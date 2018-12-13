8496a8ecb19d481592546a59a77708cdc570222b
Timmy Ryuu <sd6473170@gmail.com>
2018-12-13 13:53:41 +0800

    first submit
    first submit for project voidarea

---
+++ src/components/AuthorDetail.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: []
+    }
+  },
+  created () {
+  }
+},
+  methods () {
+
+}
+</script>
+
+<style lang="css" scoped>
+  
+</style>
